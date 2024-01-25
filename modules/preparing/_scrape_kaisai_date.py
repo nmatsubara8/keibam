e6fe7fcd95ccd96adf9149641db43120ce9bad6b
nmatsubara8 <nmatsubara8@hotmail.com>
2024-01-26 00:23:20 +0900

    pytestを利用するための環境変数の設定中

--- modules/preparing/_scrape_kaisai_date.py
+++ modules/preparing/_scrape_kaisai_date.py
@@ -1,15 +1,6 @@
-import re
-import time
-from urllib.request import urlopen
-
-import pandas as pd
-from bs4 import BeautifulSoup
-from tqdm.auto import tqdm
-from modules.constants import UrlPaths
-
-
 def scrape_kaisai_date(from_: str, to_: str):
     return 0
+
 
 def existing_check(from_: str, to_: str):
     return 0
--- tests/preparing/test_scrape_kaisai_date.py
+++ tests/preparing/test_scrape_kaisai_date.py
@@ -1,9 +1,9 @@
-import pytest
 from modules.preparing._scrape_kaisai_date import scrape_kaisai_date
 
-#@pytest.fixture()
-#def test_date():
+# @pytest.fixture()
+# def test_date():
 #  return
 
+
 def test_init():
-  assert scrape_kaisai_date("2020-01-01", "2021-01-01") == 0
+    assert scrape_kaisai_date("2020-01-01", "2021-01-01") == 0
